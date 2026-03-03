ac8da55c4086318618fd6b61e034f6fd6546ef84
John Alexander DG. Galang <galangjohnalexander45@gmail.com>
2026-03-03 12:07:50 +0800

    Configure for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./assets/components/home";
 import Login from "./assets/components/login";
 import Signup from "./assets/components/signup";
-import LandingPage from "./assets/components/landingpage"; // ADD THIS IMPORT
+import LandingPage from "./assets/components/landingpage";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/IT325-FacialRecognition">
+      {" "}
+      {/* ADD THIS */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/landingpage" element={<LandingPage />} />{" "}
-        {/* ADD THIS ROUTE */}
+        <Route path="/landingpage" element={<LandingPage />} />
       </Routes>
     </Router>
   );
